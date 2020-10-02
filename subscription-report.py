4e5b84cfc6ebad2447248268c44a16e6e45a8da3
Sebastian Hetze <shetze@redhat.com>
2020-10-02 10:55:40 +0200

    add simple subscription-report.py example script

---
+++ subscription-report.py
@@ -0,0 +1,30 @@
+import requests
+
+username="JohnDoe"
+password="I am not going to disclose my credential, not in 1000 years!"
+
+
+session = requests.Session()
+session.auth = (username, password)
+
+ownerUrl = 'https://subscription.rhn.redhat.com/subscription/users/%s/owners/' % (username)
+owners = session.get(ownerUrl, verify='/etc/rhsm/ca/redhat-uep.pem')
+
+
+parsed = owners.json()
+for owner in parsed :
+    ownerKey = owner['key']
+
+entitlementUrl = 'https://subscription.rhn.redhat.com/subscription/owners/%s/entitlements' % (ownerKey)
+entitlements = session.get(entitlementUrl)
+parsed = entitlements.json()
+print('consumer;contract;endDate')
+for entitlement in parsed :
+    consumer = entitlement['consumer']
+    name = consumer['name']
+    pool = entitlement['pool']
+    contract = pool['contractNumber']
+    endDate = entitlement['endDate']
+    # print('consumer %s contract %s endDate %s' % (name,contract,endDate))
+    print('%s;%s;%s' % (name,contract,endDate))
+
